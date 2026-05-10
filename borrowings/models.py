240c918447cd85068994486bedb5cbaccc8eefdb
SSSSSADO <grushkovik.danil@gmail.com>
2026-05-10 15:54:22 +0300

    Created Borrowing model

--- borrowings/models.py
+++ borrowings/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Borrowing(models.Model):
+    borrow_date = models.DateField()
+    expected_return_date = models.DateField()
+    actual_return_date = models.DateField()
+    book = models.ForeignKey(
+        "Book", on_delete=models.CASCADE, related_name="borrowed_books"
+    )
+    user = models.ForeignKey(
+        "User", on_delete=models.CASCADE, related_name="borrowed_users"
+    )
--- library_api_service/urls.py
+++ library_api_service/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/books/", include("books.urls", namespace="books")),
     path("api/users/", include("users.urls", namespace="users")),
+    path("api/borrowings/", include("borrowings.urls", namespace="borrowings")),
     path("api/token/", TokenObtainPairView.as_view()),
     path("api/token/refresh/", TokenRefreshView.as_view()),
 ]
