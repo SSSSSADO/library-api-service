9e10051a23ca38261e4b2599464e8675a27649cb
SSSSSADO <grushkovik.danil@gmail.com>
2026-05-10 12:54:18 +0300

    Added User model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    email = models.CharField(max_length=255)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    is_staff = models.BooleanField(default=False)
