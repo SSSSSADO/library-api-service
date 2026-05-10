76f08a69ec683f5b180fbfef04173e551bd18ab9
SSSSSADO <grushkovik.danil@gmail.com>
2026-05-10 13:47:18 +0300

    Registered User model in the admin panel

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from users.models import User
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    model = User
+
+    list_display = ("id", "email", "first_name", "last_name", "is_staff")
+    list_filter = ("is_staff", "is_superuser", "is_active")
+
+    search_fields = ("email", "first_name", "last_name")
+
+    ordering = ("id",)
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Personal info", {"fields": ("first_name", "last_name")}),
+        ("Permissions", {"fields": ("is_staff", "is_superuser", "is_active")}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("email", "first_name", "last_name", "password1", "password2", "is_staff"),
+        }),
+    )
