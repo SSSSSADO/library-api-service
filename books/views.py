d7bbf44f186de8f5f448a04e026b0ba5fd76b3f9
SSSSSADO <grushkovik.danil@gmail.com>
2026-05-10 12:38:51 +0300

    Added permissions for book service

--- books/views.py
+++ books/views.py
@@ -1,4 +1,8 @@
+from django.contrib.auth.models import Permission
+
 from rest_framework import viewsets
+from rest_framework.permissions import IsAdminUser, AllowAny
+from rest_framework.serializers import Serializer
 
 from books.serializers import BookListSerializer, BookDetailSerializer
 from books.models import Book
@@ -7,7 +11,13 @@ from books.models import Book
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
 
-    def get_serializer_class(self):
+    def get_serializer_class(self) -> Serializer:
         if self.action == "list":
             return BookListSerializer
         return BookDetailSerializer
+
+    def get_permissions(self) -> list[Permission]:
+        permission_classes = [IsAdminUser]
+        if self.action in ["list", "retrieve"]:
+            permission_classes = [AllowAny]
+        return [permission() for permission in permission_classes]
